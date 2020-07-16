0fd5cb494317a7cb058998cf7f1e9cccb7551f75
NRohrer <nicholas.rohrer@thetiebar.com>
2020-07-16 16:29:25 -0500

    product data for plp

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,38 +1,38 @@
-exports.createPages = async ({ actions: {createPage }, graphql }) => {
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
+  // page with no data
+  createPage({
+    path: "/no-data/",
+    component: require.resolve("./src/templates/no-data.js"),
+  })
 
-	// page with no data
-	createPage({
-		path: "/no-data/",
-		component: require.resolve("./src/templates/no-data.js")
-	})
+  // page with hard coded context data
+  createPage({
+    path: "/page-with-context/",
+    component: require.resolve("./src/templates/with-context.js"),
+    context: {
+      title: "This is the context title",
+      content:
+        "<p>This is the context content</p><p>This is coming from the gatsby node file and formatted as html.</p>",
+    },
+  })
 
-	// page with hard coded context data
-	createPage({
-		path: "/page-with-context/",
-		component: require.resolve("./src/templates/with-context.js"),
-		context: {
-			title: "This is the context title",
-			content: '<p>This is the context content</p><p>This is coming from the gatsby node file and formatted as html.</p>'
-		}
-	})
+  // product pages with json context data
+  const products = require("./data/products.json")
+  products.forEach(product => {
+    createPage({
+      path: `/product/${product.slug}`,
+      component: require.resolve("./src/templates/product.js"),
+      context: {
+        title: product.title,
+        description: product.description,
+        image: product.image,
+        price: product.price,
+      },
+    })
+  })
 
-	// product pages with json context data
-	const products = require("./data/products.json")
-	products.forEach(product => {
-		createPage({
-			path: `/product/${product.slug}`,
-			component: require.resolve("./src/templates/product.js"),
-			context: {
-				title: product.title,
-				description: product.description,
-				image: product.image,
-				price: product.price
-			}
-		})
-	})
-
-	// product pages with graphql data
-	const results = await graphql(`
+  // product pages with graphql data
+  const results = await graphql(`
     {
       allProductsJson {
         edges {
@@ -52,60 +52,60 @@ exports.createPages = async ({ actions: {createPage }, graphql }) => {
         slug: product.slug,
       },
     })
-	})
+  })
 
-	var axios = require('axios');
-	const PLPs = ["shirts", "pants", "ties"]
+  var axios = require("axios")
+  const PLPs = ["shirts", "pants", "ties"]
 
-	// configuration for token api call
-	var tokenConfig = {
-		method: 'get',
-		url: 'https://web-dev.thetiebar.com/api/token',
-		headers: {
-			'x-client-id': '7c1f4a77f8f7443bb0d0af8fca9f27f8',
-			'Cookie': 'moov_bucket=79; .ASPXANONYMOUS=kgDGsKVYEQQM6ymrXI8HRZdnKM4yxvf1Ot2QduvA_Vc6_ddKa64eYgXH50EbBxOLtuReFsCmIBfeezONm6Pp4WCZx9e7yB1qWa5d6IknLxUEP0xA9H3xWE_OMx0RimQNVXfOZA2; moov_=3b370d15-6234-4ebe-bfcd-05ab980e4040'
-		}
-	};
+  // configuration for token api call
+  var tokenConfig = {
+    method: "get",
+    url: "https://web-dev.thetiebar.com/api/token",
+    headers: {
+      "x-client-id": "7c1f4a77f8f7443bb0d0af8fca9f27f8",
+      Cookie:
+        "moov_bucket=79; .ASPXANONYMOUS=kgDGsKVYEQQM6ymrXI8HRZdnKM4yxvf1Ot2QduvA_Vc6_ddKa64eYgXH50EbBxOLtuReFsCmIBfeezONm6Pp4WCZx9e7yB1qWa5d6IknLxUEP0xA9H3xWE_OMx0RimQNVXfOZA2; moov_=3b370d15-6234-4ebe-bfcd-05ab980e4040",
+    },
+  }
 
-	// token api call
-	axios(tokenConfig)
-	.then(function (response) {
-		console.log(JSON.stringify(response.data));
+  // token api call
+  var token = await axios(tokenConfig)
+    .then(function (response) {
+      //console.log(JSON.stringify(response.data));
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 
-		PLPs.forEach(plp => {
+  // configuration for PLP api calls
+  let plpConfig = {
+    method: "get",
+    url: `https://web-dev.thetiebar.com/api/products/shirts`,
+    headers: {
+      "x-client-id": "7c1f4a77f8f7443bb0d0af8fca9f27f8",
+      "x-Access-Token": token,
+      Cookie:
+        "moov_bucket=79; .ASPXANONYMOUS=kgDGsKVYEQQM6ymrXI8HRZdnKM4yxvf1Ot2QduvA_Vc6_ddKa64eYgXH50EbBxOLtuReFsCmIBfeezONm6Pp4WCZx9e7yB1qWa5d6IknLxUEP0xA9H3xWE_OMx0RimQNVXfOZA2; moov_=3b370d15-6234-4ebe-bfcd-05ab980e4040",
+    },
+  }
 
-			// configuration for PLP api calls
-			var plpConfig = {
-				method: 'get',
-				url: `https://web-dev.thetiebar.com/api/products/${plp}`,
-				headers: {
-					'x-client-id': '7c1f4a77f8f7443bb0d0af8fca9f27f8',
-					'x-Access-Token': response.data,
-					'Cookie': 'moov_bucket=79; .ASPXANONYMOUS=kgDGsKVYEQQM6ymrXI8HRZdnKM4yxvf1Ot2QduvA_Vc6_ddKa64eYgXH50EbBxOLtuReFsCmIBfeezONm6Pp4WCZx9e7yB1qWa5d6IknLxUEP0xA9H3xWE_OMx0RimQNVXfOZA2; moov_=3b370d15-6234-4ebe-bfcd-05ab980e4040'
-				}
-			};
+  // PLP api call
+  const products1 = await axios(plpConfig)
+    .then(function (response) {
+      //console.log(JSON.stringify(response.data));
+      return response.data.result.products
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 
-			// PLP api call
-			axios(plpConfig)
-			.then(function (response) {
-				console.log(JSON.stringify(response.data));
-
-				// create PLP for each item in PLP list
-				createPage({
-					path: `/${plp}/`,
-					component: require.resolve("./src/templates/product-listing.js"),
-					context: {
-						item: "test"
-					},
-				})
-
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-		})
-	})
-	.catch(function (error) {
-		console.log(error);
-	});
+  // create PLP for each item in PLP list
+  createPage({
+    path: `/shirts/`,
+    component: require.resolve("./src/templates/product-listing.js"),
+    context: {
+      items: products1,
+    },
+  })
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import { withStyles, Grid, Typography, Button } from '@material-ui/core'
 import indexStyles from '../styles/index.module.css'
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
