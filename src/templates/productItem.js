0e912543a923a68c8722fa27ae2e1408a964414f
NRohrer <nicholas.rohrer@thetiebar.com>
2020-07-21 15:39:14 -0500

    support for links to each pdp from plp

--- src/templates/productItem.js
+++ src/templates/productItem.js
@@ -1,9 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const ProductItem = ({product}) => {
 	return(
 		<div>
-			<img src={`${product.productImage}?h=310`} alt={product.title} />
+			<Link to={`${product.masterUrl}`}>
+				<img src={`${product.productImage}?h=310`} alt={product.title} />
+			</Link>
 			<h5>{product.title}</h5>
 			<p>{product.priceInfo}</p>
 		</div>
